ba2fa9b46884c64815c5e41b84877f09d25f6442
asiakostrzewa <joanna.o.kostrzewa@tlen.pl>
2020-08-22 15:32:52 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-nnpyq3
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-nnpyq3)
---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { StudentsComponent } from './students/students.component';
+import { AppRoutingModule } from './app-routing.module';
+import { StudentEditComponent } from './student-edit/student-edit.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    StudentsComponent,
+    StudentEditComponent,
+    NotFoundComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
